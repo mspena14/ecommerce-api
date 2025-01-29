d96bd81a96fbf5f40e1afc90e013168e5496faa6
mspena14 <marlono1naranjo@gmail.com>
2025-01-29 16:31:17 -0500

    feat: add database configuration service for TypeORM with PostgreSQL support

---
+++ src/common/config/db-config.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { TypeOrmOptionsFactory, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+import { User } from 'src/users/entities/user.entity';
+import { Cart } from 'src/carts/entities/cart.entity';
+import { CartItem } from 'src/cart-items/entities/cart-item.entity';
+import { Payment } from 'src/payments/entities/payment.entity';
+import { Product } from 'src/products/entities/product.entity';
+import { Reservation } from 'src/reservations/entities/reservation.entity';
+import { Schedule } from 'src/schedules/entities/schedule.entity';
+import { Service } from 'src/services/entities/service.entity';
+
+
+@Injectable()
+export class DatabaseConfigService implements TypeOrmOptionsFactory {
+  constructor(private readonly configService: ConfigService) {}
+
+  createTypeOrmOptions(): Promise<TypeOrmModuleOptions> | TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+      host: this.configService.get<string>('DB_HOST'),
+      port: this.configService.get<number>('DB_PORT'),
+      username: this.configService.get<string>('DB_USER'),
+      password: this.configService.get<string>('DB_PASSWORD'),
+      database: this.configService.get<string>('DB_NAME'),
+      
+      entities: [
+        User,
+        Cart,
+        CartItem,
+        Payment,
+        Product,
+        Reservation,
+        Schedule,
+        Service
+      ],
+    };
+  }
+}

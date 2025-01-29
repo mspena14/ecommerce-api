859fe0b7a219bde9f02fa937024eca5d6012c5e5
mspena14 <marlono1naranjo@gmail.com>
2025-01-29 15:08:04 -0500

    feat: implement product module with create and update DTOs, product entity, service, and controller

---
+++ src/products/entities/product.entity.ts
@@ -0,0 +1,33 @@
+import { CartItem } from "src/cart-items/entities/cart-item.entity";
+import { AuditableEntity } from "src/common/entites/auditable.entity";
+import { ImageInterface } from "src/common/interfaces/images.interface";
+import { User } from "src/users/entities/user.entity";
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Product extends AuditableEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('text', { nullable: false })
+  name: string;
+
+  @Column('text', { nullable: false })
+  description: string;
+
+  @Column('decimal', { nullable: false })
+  price: number;
+
+  @Column('int', { nullable: false })
+  stock: number;
+
+  @Column('jsonb', { nullable: true })
+  images: ImageInterface;
+
+  @ManyToOne(() => User, (user) => user.products)
+  @JoinColumn({ name: 'seller_id' })
+  seller: User;
+
+  @OneToMany(() => CartItem, (cartItem) => cartItem.product, { cascade: true })
+  cartItems: CartItem[];
+}

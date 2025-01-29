6673b6bbfd3d93536a69a230660cd791ab900fc6
mspena14 <marlono1naranjo@gmail.com>
2025-01-29 14:57:45 -0500

    refactor: remove unused app service and controller, update module imports, and configure global settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+import { ServicesModule } from './services/services.module';
+import { SchedulesModule } from './schedules/schedules.module';
+import { ReservationsModule } from './reservations/reservations.module';
+import { CartsModule } from './carts/carts.module';
+import { CartItemsModule } from './cart-items/cart-items.module';
+import { PaymentsModule } from './payments/payments.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule, ProductsModule, ServicesModule, SchedulesModule, ReservationsModule, CartsModule, CartItemsModule, PaymentsModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpErrorFilter } from './common/filters/http-exceptions.filter';
+import { ResponseFormatInterceptor } from './common/interceptors/response-format.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.setGlobalPrefix('api');
+  app.useGlobalFilters(new HttpErrorFilter());
+  app.useGlobalInterceptors(new ResponseFormatInterceptor);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();

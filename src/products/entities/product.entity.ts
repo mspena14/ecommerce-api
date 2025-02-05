6b0720cca6ad2a5ac1dd0f89b2dca2d471ef998e
mspena14 <marlono1naranjo@gmail.com>
2025-02-05 15:43:32 -0500

    feat: add category field to Product entity using CategoriesEnum

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,8 +1,9 @@
 import { CartItem } from "src/cart-items/entities/cart-item.entity";
 import { AuditableEntity } from "src/common/entites/auditable.entity";
+import { CategoriesEnum } from "src/common/enums/categories.enum";
 import { ImageInterface } from "src/common/interfaces/images.interface";
 import { User } from "src/users/entities/user.entity";
-import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity('products')
 export class Product extends AuditableEntity {
@@ -23,6 +24,9 @@ export class Product extends AuditableEntity {
 
   @Column('jsonb', { nullable: true })
   images: ImageInterface;
+
+  @Column('enum', { enum: CategoriesEnum })
+  category: CategoriesEnum;
 
   @ManyToOne(() => User, (user) => user.products)
   @JoinColumn({ name: 'seller_id' })
